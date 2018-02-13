3696e72591e65653ae87736fa2ae70138aee0594
Esraa Mostfa <esraamostfa13@gmail.com>
2018-02-14 01:47:06 +0200

    Add entities

---
+++ forums/main.py
@@ -0,0 +1,8 @@
+import models
+
+member1 = models.Member("Mohamad", 23)
+Member2 = models.Member("Ahmad", 25)
+
+post1 = models.Post("post 1", "this is post 1")
+post2 = models.Post("post 2", "this is post 2")
+post3 = models.Post("post 3", "this is post 3")
