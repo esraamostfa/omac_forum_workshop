37a9a83dc2e05f20f4ae9f66de90a45ecffff1eb
Esraa Mostfa <esraamostfa13@gmail.com>
2018-02-18 22:54:41 +0200

    Organize the main file

--- forums/main.py
+++ forums/main.py
@@ -1,44 +1,88 @@
 import models
 import stores
 
-member1 = models.Member("Mohamad", 23, id)
-member2 = models.Member("Ahmad", 25, id)
+#----------------------define functions for tests-------------------
 
-post1 = models.Post("post 1", "this is post 1")
-post2 = models.Post("post 2", "this is post 2")
-post3 = models.Post("post 3", "this is post 3")
+def creat_members():
+
+	member1 = models.Member("Mohammad", 23, id)
+	member2 = models.Member("Ahmad", 25, id)
+	print(member1)
+	print(member2)
+	print("=" * 30)
+
+	return member1, member2
+
+def creat_posts():
+
+	post1 = models.Post("post 1", "this is post 1")
+	post2 = models.Post("post 2", "this is post 2")
+	post3 = models.Post("post 3", "this is post 3")
+	print(post1)
+	print(post2)
+	print(post3)
+	print("=" * 30)
+
+	return post1, post2, post3
+
+def add_members_to_store(member_instances, members_store):
+
+	for member in member_instances:
+		members_store.add(member)
+
+def add_posts_to_store(post_instances, posts_store):
+
+	for post in post_instances:
+		posts_store.add(post)
+
+def print_all_members(members_store):
+	print(members_store.get_all())
+	print("=" * 30)
+
+def get_member_by_id(members_store, id):
+	print(members_store.get_by_id(id))
+	print("=" * 30)
+
+def delete_member(members_store, member):
+	members_store.delete(member.id)
+	print(members_store.entity_exists(member))
+	print("=" * 30)
+
+def update_member(members_store ,member):
+
+	member_copy = models.Member(member.name, member.age, member.id)
+	members_store.update(member_copy, 'Mazen', 21)
+	print(member.name, member.age)
+	print(member_copy.name, member_copy.age)
+	print("=" * 30)
+
+def update_post(posts_store, post):
+
+	post_copy = models.Post(post.title, post.subject)
+	posts_store.update(post_copy, "post copy", "this is post copy")
+	print(post.title + ":" , post.subject)
+	print(post_copy.title + ":", post_copy.subject)
+	print("=" * 30)
+	
+def get_member_by_name(members_store, name):	
+	print(members_store.get_by_name(name))
+
+#--------------------------call functions for tests------------------------
 
 members_store = stores.MembersStore()
 posts_store = stores.PostsStore()
+member_instances = creat_members()
+member1, member2 = member_instances
+post_instances = creat_posts()
+post1, post2, post3 = post_instances
 
-members_store.add(member1)
-members_store.add(member2)
-
-posts_store.add(post1)
-posts_store.add(post2)
-posts_store.add(post3)
-
-members_store.get_all()
-posts_store.get_all()
-
-members_store.get_by_id(1)
-members_store.get_by_id(2)
-
-print(members_store.entity_exists(member1))
-print(members_store.entity_exists(member2))
-
-members_store.delete(2)
-print(members_store.entity_exists(member2))
-
-member1_copy = models.Member(member1.name, member1.age, member1.id)
-members_store.update(member1_copy, 'Mazen', 21)
-print(member1.name, member1.age)
-print(member1_copy.name, member1_copy.age)
-
-post1_copy = models.Post(post1.title, post1.subject)
-posts_store.update(post1_copy, "post 1 copy", "this is post 1 copy")
-print(post1.title + ":" , post1.subject)
-print(post1_copy.title + ":", post1_copy.subject)
-		
-print(members_store.get_by_name('Mohamad'))
-print(members_store.get_by_name('Ahmad'))
+creat_members()
+creat_posts()
+add_members_to_store(member_instances, members_store)
+add_posts_to_store(post_instances, posts_store)
+print_all_members(members_store)
+get_member_by_id(members_store, 1)
+delete_member(members_store, member2)
+update_member(members_store ,member1)
+update_post(posts_store, post1)
+get_member_by_name(members_store, "Mohammad")
